1e3eceb038a38197202452fd0b2c4c076dc01865
leysa Preguntegui <leysajimena@gmail.com>
2016-09-18 15:11:22 -0600

    Primera Version

---
+++ tutorial-maven-eclipse/src/main/java/pe/edu/upc/tutorial_maven_eclipse/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.tutorial_maven_eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
